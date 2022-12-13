ddd4ef9ea0ccf5b2aa3246a1914792206f17f28e
NATSU03 <vishwasw75@gmail.com>
2022-12-13 12:29:35 +0530

    axios try

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,5 +1,7 @@
 const urlModel=require('../Model/urlModel')
 const shortId=require('shortid')
+const axios  = require("axios")
+
 
 let isvalidUrl= /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
 
@@ -12,25 +14,49 @@ try{
         return res.status(400).send({status:false, message:"Please provide longUrl"})
     }
     let {longUrl}=data
+
+
+    let urlfound = false
+
+    // let checkvalidUrl = await axios.get(longUrl)
+    // .then((res)=>{if (res.status == 200 || res.status == 201) urlfound = true})
+    // .catch(()=>{})
+
+    // if (urlfound == false){
+    //     return res.status(400).send({status : false , message : "invalid URL"})
+    // }
+
+    // if(!checkvalidUrl) return res.status(404).send({status:false,message:"Url not found"})
+
     if(longUrl){
-    if(!isvalidUrl.test(longUrl)){
+        if(!isvalidUrl.test(longUrl)){
         return res.status(400).send({status:false,message:"Please Provide Valid Url"})
     }else{
         let alreadypresent=await urlModel.findOne({longUrl:longUrl})
+
         if(alreadypresent){
+
            var urlCode=alreadypresent.urlCode
+           if(!shortId.isValid(urlCode)) return res.status(400).send({status:false,message:"Invalid Short Url"})
+           var url = {longUrl,shortUrl:`https://localhost:3000/${urlCode.toLowerCase()}`,urlCode}
+           return res.status(200).send({status:true,message:"url",data:url})
+
         }else{
             var urlCode= shortId.generate()
+            var url = {longUrl,shortUrl:`https://localhost:3000/${urlCode.toLowerCase()}`,urlCode}
+            await urlModel.create(url)
+            return res.status(200).send({status:true,message:"url",data:url})
         }
     }
 }
-    if(!shortId.isValid(urlCode)) return res.status(400).send({status:false,message:"Invalid Short Url"})
-    if(!urlCode) return res.status(400).send({status:false, message:"urlcode not present"})
-    let url={longUrl,shortUrl:`https://localhost:3000/${urlCode.toLowerCase()}`,urlCode}
-    await urlModel.create(url)
-    return res.status(200).send({status:true,message:"url",data:url})
+
+    // let url={longUrl,shortUrl:`https://localhost:3000/${urlCode.toLowerCase()}`,urlCode}
+    // await urlModel.create(url)
+
+    //return res.status(200).send({status:true,message:"url",data:url})
+
 }catch(error){
-    return res.status(500).send({status:false,message:error.message})
+       return res.status(500).send({status:false,message:error.message})
 }
 }
 
