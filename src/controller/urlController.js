d0de714a98b1d795ce37675c2038e3b9b2924aed
ShashiS07 <shashiss0708@gmail.com>
2022-12-15 21:22:31 +0530

    expiry done

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -50,19 +50,19 @@ try{
         return res.status(400).send({status : false , message : "invalid URL"})
     }
 
-    let urlCode=shortId.generate().toLowerCase()
     let checkincache=await GET_ASYNC(`${longUrl}`)
     if(checkincache){
         let urlData=JSON.parse(checkincache)
-
+        
         return res.status(201).send({status:true, message:"URL is already shortened",data:urlData})
     }
     let urlpresent= await urlModel.findOne({longUrl}).select({ createdAt: 0, updatedAt: 0, __v: 0, _id: 0 })
     if(urlpresent){
         await SET_ASYNC(`${longUrl}`,JSON.stringify(urlpresent),"EX",300);
-
+        
         return res.status(201).send({status:true, message:"URL is already shortened",data:urlpresent})
     }
+    let urlCode=shortId.generate().toLowerCase()
     let baseUrl="https://localhost:3000/"
     let shortUrl=`${baseUrl}${urlCode}`
     let url={longUrl,shortUrl,urlCode}
