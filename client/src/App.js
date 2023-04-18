8fdcf210b3d67a9fb5189163c96ffd270c9a6d68
ShashiS07 <shashiss0708@gmail.com>
2023-04-18 12:37:36 +0530

    Fronted Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
+  const [longUrl, setLongUrl] = useState("")
+  const [shortUrl, setShortUrl] = useState("")
+  const handleUrl = (event) => {
+    event.preventDefault()
+    axios.post('http://localhost:4000/url/shorten', { longUrl, shortUrl })
+      .then((res) => {
+        setShortUrl(res.data.data.shortUrl)
+        // alert(res.data.message)
+        setLongUrl("")
+      })
+
+      .catch((err) => alert(err.response.data.message))
+  }
+
+  const handleCopy=()=>{
+    if(shortUrl===""){
+      alert("Short Link is Empty")
+    }else{
+      alert("Link is Copied")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <header>Create Short Link</header>
+      </div>
+      <div className='long-url'>
+        Enter URL: <input type='text' placeholder='http://site.com' value={longUrl} onChange={(e) => setLongUrl(e.target.value)} />
+        <div className='button'>
+          <button onClick={handleUrl}>Get Shorten URL</button>
+        </div>
+        <input type='text' placeholder='ShortLink will be here' value={shortUrl} onChange={(e) => setShortUrl}></input>
+        <CopyToClipboard text={shortUrl}>
+          <button onClick={handleCopy}>Copy Above Link</button>
+        </CopyToClipboard>
+      </div>
+
     </div>
   );
 }
