52770df4a645452f6706cb8be757a6c18eea0a19
ShashiS07 <shashiss0708@gmail.com>
2023-04-24 17:16:30 +0530

    back to previous host

--- server/src/controller/urlController.js
+++ server/src/controller/urlController.js
@@ -63,7 +63,7 @@ try{
         return res.status(201).send({status:true, message:"URL is already shortened",data:urlpresent})
     }
     let urlCode=shortId.generate().toLowerCase()
-    let baseUrl="https://url-shortner-backend-hnb3.onrender.com/"
+    let baseUrl="http://localhost:4000/"
     let shortUrl=`${baseUrl}${urlCode}`
     let url={longUrl,shortUrl,urlCode}
 
