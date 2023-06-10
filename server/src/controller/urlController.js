f7fc15e0cc5fa0754aa43f9342541920f1572620
ShashiS07 <shashiss0708@gmail.com>
2023-06-10 11:57:15 +0530

    changed url

--- server/src/controller/urlController.js
+++ server/src/controller/urlController.js
@@ -63,7 +63,7 @@ try{
         return res.status(201).send({status:true, message:"URL is already shortened",data:urlpresent})
     }
     let urlCode=shortId.generate().toLowerCase()
-    let baseUrl="http://localhost:4000/"
+    let baseUrl="https://url-shortner-backend-q545.onrender.com/"
     let shortUrl=`${baseUrl}${urlCode}`
     let url={longUrl,shortUrl,urlCode}
 
